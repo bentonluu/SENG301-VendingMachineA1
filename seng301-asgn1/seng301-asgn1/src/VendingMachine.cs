c697b166f63541e5257e064a9cb469e790134c06
Benton Luu <benton.luu@ucalgary.ca>
2017-09-25 22:39:20 -0600

    Add files via upload

---
+++ seng301-asgn1/seng301-asgn1/src/VendingMachine.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Frontend1;
+using System.Collections;
+
+namespace seng301_asgn1.src
+{
+    public class VendingMachine
+    {
+        private List<int> vm_coinTypes;
+        private int vm_buttons;
+        private List<string> vm_popNames;
+        private List<int> vm_popCosts;
+        private List<Coin>[] vm_CoinChute;
+        private List<Pop>[] vm_PopChute;
+        private List<int> insertedCoins = new List<int>();
+        private List<Deliverable> vm_DeliverChute = new List<Deliverable>();
+        private List<Coin> vm_Earned = new List<Coin>();
+        private List<Coin> vm_unloadCoin = new List<Coin>();
+        private List<Pop> vm_unloadPop = new List<Pop>();
+        private List<Coin> vm_insertChute = new List<Coin>();
+
+        public VendingMachine(List<int> coinKinds, int selectionButtonCount)
+        {
+            this.vm_coinTypes = coinKinds;
+            this.vm_buttons = selectionButtonCount;
+        }
+
+        public List<int> get_vmCoins()
+        {
+            return vm_coinTypes;
+        }
+
+        public int get_vmButtons()
+        {
+            return vm_buttons;
+        }
+
+        public void pop_Names(List<string> popnames)
+        {
+            this.vm_popNames = popnames;
+        }
+
+        public List<string> get_vmPopNames()
+        {
+            return this.vm_popNames;
+        }
+
+        public void pop_Costs(List<int> popcosts)
+        {
+            this.vm_popCosts = popcosts;
+        }
+
+        public List<int> get_vmPopCosts()
+        {
+            return vm_popCosts;
+        }
+
+        public void createChutes(int numCoinTypes, int numPopTypes)
+        {
+            vm_CoinChute = new List<Coin>[numCoinTypes];
+            vm_PopChute = new List<Pop>[numPopTypes];
+        }
+
+        public void loadCoinChute(int coinIndex, List<Coin> coinlist)
+        {
+            foreach (Coin coin in coinlist)
+            {
+                vm_CoinChute[coinIndex].Add(coin);
+            }
+
+        }
+
+        public void nullCoinChute(int chuteIndex)
+        {
+            vm_CoinChute[chuteIndex] = new List<Coin>();
+        }
+
+        public void nullPopChute(int chuteIndex)
+        {
+            vm_PopChute[chuteIndex] = new List<Pop>();
+        }
+
+        public List<Coin>[] get_CoinChute()
+        {
+            return vm_CoinChute;
+        }
+
+        public void loadPopChute(int popIndex, List<Pop> poplist)
+        {
+            foreach (Pop pop in poplist)
+            {
+                vm_PopChute[popIndex].Add(pop);
+            }
+        }
+
+        public List<Pop>[] get_PopChute()
+        {
+            return vm_PopChute;
+        }
+
+        public void insertCoinEarn()
+        {
+            foreach (Coin coin in vm_insertChute)
+            {
+                vm_Earned.Add(coin);
+            }
+        }
+
+        public void clear_insertCoins()
+        {
+            vm_insertChute.Clear();
+            insertedCoins.Clear();
+        }
+
+        public void insertChute(Coin coin)
+        {
+            vm_insertChute.Add(coin);
+        }
+
+        public void insertCoin(int coin)
+        {
+            insertedCoins.Add(coin);
+        }
+
+        public int insertCoinTotal()
+        {
+            int totalSum = insertedCoins.Sum();
+            return totalSum;
+        }
+
+        public void deliveryChuteCoin(Coin coin)
+        {
+            vm_DeliverChute.Add(coin);
+        }
+
+        public void give_Change(int change)
+        {
+            int tempChange = change;
+            List<int> tempCoinTypes = new List<int>();
+            foreach (int coin in vm_coinTypes)
+            {
+                tempCoinTypes.Add(coin);
+            }
+            tempCoinTypes.Sort();
+            tempCoinTypes.Reverse();
+
+            List<int> coinIndex = new List<int>();
+            for (int p = 0; p < vm_coinTypes.Count; p++)
+            {
+                coinIndex.Add(vm_coinTypes.IndexOf(tempCoinTypes[p]));
+            }
+
+            List<int> amtCoinChute = new List<int>();
+            foreach (int index in coinIndex)
+            {
+                amtCoinChute.Add(vm_CoinChute[index].Count());
+            }
+
+            for (int i = 0; i < vm_CoinChute.Count(); i++)
+            {
+                if (amtCoinChute[i] >= 1)
+                {
+                    if (change == (vm_coinTypes[coinIndex[i]] * amtCoinChute[i]))
+                    {
+                        for (int j = 0; j < amtCoinChute[i]; j++)
+                        {
+                            vm_DeliverChute.Add(vm_CoinChute[coinIndex[i]][0]);
+                            vm_CoinChute[coinIndex[i]].RemoveAt(0);
+                        }
+                        tempChange = 0;
+                    }
+
+                    else
+                    {
+                        for (int k = 1; k <= amtCoinChute[i]; k++)
+                        {
+
+                            tempChange = tempChange - vm_coinTypes[coinIndex[i]];
+
+                            if (tempChange > 0)
+                            {
+                                vm_DeliverChute.Add(vm_CoinChute[coinIndex[i]][0]);
+                                vm_CoinChute[coinIndex[i]].RemoveAt(0);
+                            }
+
+                            else if (tempChange < 0)
+                            {
+                                tempChange = tempChange + vm_coinTypes[coinIndex[i]];
+                            }
+
+                            else
+                            {
+                                vm_DeliverChute.Add(vm_CoinChute[coinIndex[i]][0]);
+                                vm_CoinChute[coinIndex[i]].RemoveAt(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void deliveryChutePop(int popType)
+        {
+            vm_DeliverChute.Add(vm_PopChute[popType][0]);
+            vm_PopChute[popType].RemoveAt(0);
+        }
+
+        public List<Deliverable> get_deliveryChute()
+        {
+            return vm_DeliverChute;
+        }
+
+        public void clear_deliveryChute()
+        {
+            vm_DeliverChute.Clear();
+        }
+
+        public List<Coin> unloadCoinChute()
+        {
+            List<int> nulltest = new List<int>();
+            int nullint = 0;
+            foreach (List<Coin> coinlist in vm_CoinChute)
+            {
+                if (coinlist == null)
+                {
+                    nulltest.Add(0);
+                }
+            }
+
+            foreach (int num in nulltest)
+            {
+                if (num == 0)
+                {
+                    nullint = nullint + 1;
+                }
+            }
+ 
+            if(nullint != vm_CoinChute.Count())
+            {
+                List<int> amtCoinChute = new List<int>();
+                int coinCounter = 0;
+
+                foreach (List<Coin> coin in vm_CoinChute)
+                {
+                    amtCoinChute.Add(coin.Count());
+                }
+
+                for (int i = 0; i < vm_CoinChute.Count(); i++)
+                {
+                    if (amtCoinChute[coinCounter] >= 1)
+                    {
+                        for (int j = 0; j < amtCoinChute[coinCounter]; j++)
+                        {
+                            vm_unloadCoin.Add(vm_CoinChute[coinCounter][0]);
+                            vm_CoinChute[coinCounter].RemoveAt(0);
+                        }
+                    }
+                    coinCounter++;
+                }
+            }
+            
+            return vm_unloadCoin;
+        }
+
+        public List<Coin> get_EarnedChute()
+        {
+            return vm_Earned;
+        }
+
+        public List<Pop> unloadPopChute()
+        {
+            List<int> amtPopChute = new List<int>();
+            int popCounter = 0;
+
+            foreach (List<Pop> pop in vm_PopChute)
+            {
+                amtPopChute.Add(pop.Count());
+            }
+
+            for (int i = 0; i < vm_PopChute.Count(); i++)
+            {
+                if (amtPopChute[popCounter] >= 1)
+                {
+                    for (int j = 0; j < amtPopChute[popCounter]; j++)
+                    {
+                        vm_unloadPop.Add(vm_PopChute[popCounter][0]);
+                        vm_PopChute[popCounter].RemoveAt(0);
+                    }
+
+                }
+                popCounter++;
+            }
+            return vm_unloadPop;
+        }
+
+        public List<IList> unloadVM()
+        {
+
+            List<IList> vm_unload = new List<IList> { unloadRemainingCoin(), unloadEarnedCoin(), unloadPop() };
+            return vm_unload;
+        }
+
+        public List<Coin> unloadRemainingCoin()
+        {
+            List<Coin> list = new List<Coin>();
+            foreach (Coin coin in unloadCoinChute())
+            {
+                list.Add(coin);
+            }
+            vm_unloadCoin.Clear();
+            return list;
+        }
+
+        public List<Coin> unloadEarnedCoin()
+        {
+            List<Coin> list = new List<Coin>();
+            foreach (Coin coin in vm_Earned)
+            {
+                list.Add(coin);
+            }
+            vm_Earned.Clear();
+            return list;
+        }
+
+        public List<Pop> unloadPop()
+        {
+            List<Pop> list = new List<Pop>();
+            foreach (Pop pop in unloadPopChute())
+            {
+                list.Add(pop);
+            }
+            vm_unloadPop.Clear();
+            return list;
+        }
+    }
+}
---
+++ seng301-asgn1/seng301-asgn1/src/VendingMachineFactory.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Frontend1;
+using seng301_asgn1.src;
+
+namespace seng301_asgn1
+{
+    /// <summary>
+    /// Represents the concrete virtual vending machine factory that you will implement.
+    /// This implements the IVendingMachineFactory interface, and so all the functions
+    /// are already stubbed out for you.
+    /// 
+    /// Your task will be to replace the TODO statements with actual code.
+    /// 
+    /// Pay particular attention to extractFromDeliveryChute and unloadVendingMachine:
+    /// 
+    /// 1. These are different: extractFromDeliveryChute means that you take out the stuff
+    /// that has already been dispensed by the machine (e.g. pops, money) -- sometimes
+    /// nothing will be dispensed yet; unloadVendingMachine is when you (virtually) open
+    /// the thing up, and extract all of the stuff -- the money we've made, the money that's
+    /// left over, and the unsold pops.
+    /// 
+    /// 2. Their return signatures are very particular. You need to adhere to this return
+    /// signature to enable good integration with the other piece of code (remember:
+    /// this was written by your boss). Right now, they return "empty" things, which is
+    /// something you will ultimately need to modify.
+    /// 
+    /// 3. Each of these return signatures returns typed collections. For a quick primer
+    /// on typed collections: https://www.youtube.com/watch?v=WtpoaacjLtI -- if it does not
+    /// make sense, you can look up "Generic Collection" tutorials for C#.
+    /// </summary>
+    public class VendingMachineFactory : IVendingMachineFactory
+    {
+
+        private List<VendingMachine> vm_List = new List<VendingMachine>();
+
+        public VendingMachineFactory()
+        {
+
+        }
+
+        public int createVendingMachine(List<int> coinKinds, int selectionButtonCount)
+        {
+
+            bool checkNeg = coinKinds.Min() <= 0;
+            if (checkNeg == true)
+            {
+                throw new Exception("Coin values are zero or negative!");
+            }
+
+            if (coinKinds.Count() != coinKinds.Distinct().Count())
+            {
+                throw new Exception("Coin values are the same!");
+            }
+
+            var vm = new VendingMachine(coinKinds, selectionButtonCount);
+            vm_List.Add(vm);
+            int vm_ID = vm_List.Count() - 1;
+
+            return vm_ID;
+        }
+
+        public void configureVendingMachine(int vmIndex, List<string> popNames, List<int> popCosts)
+        {
+            bool checkNeg = popCosts.Min() <= 0;
+            if (checkNeg == true)
+            {
+                throw new Exception("Cost of a pop is zero or negative!");
+            }
+
+            if (popCosts.Count() != popNames.Count())
+            {
+                throw new Exception("The number of pop names is different from the number of pop costs!");
+            }
+
+            if (popNames.Count() != vm_List[vmIndex].get_vmButtons())
+            {
+                throw new Exception("The number of pop names is not equal to the number of buttons for the vending machine!");
+            }
+
+            vm_List[vmIndex].pop_Names(popNames);
+            vm_List[vmIndex].pop_Costs(popCosts);
+
+            int numcoinTypes = vm_List[vmIndex].get_vmCoins().Count();
+            int numpopTypes = popNames.Count();
+            vm_List[vmIndex].createChutes(numcoinTypes, numpopTypes);
+
+            for (int chuteIndex = 0; chuteIndex < vm_List[vmIndex].get_vmCoins().Count; chuteIndex++)
+            {
+                if (vm_List[vmIndex].get_CoinChute()[chuteIndex] == null)
+                {
+                    vm_List[vmIndex].nullCoinChute(chuteIndex);
+                }
+            }
+
+            for (int chuteIndex = 0; chuteIndex < vm_List[vmIndex].get_vmButtons(); chuteIndex++)
+            {
+                if (vm_List[vmIndex].get_PopChute()[chuteIndex] == null)
+                {
+                    vm_List[vmIndex].nullPopChute(chuteIndex);
+                }
+            }
+        }
+
+        public void loadCoins(int vmIndex, int coinKindIndex, List<Coin> coins)
+        {
+            vm_List[vmIndex].loadCoinChute(coinKindIndex, coins);
+        }
+
+        public void loadPops(int vmIndex, int popKindIndex, List<Pop> pops)
+        {
+
+            vm_List[vmIndex].loadPopChute(popKindIndex, pops);
+        }
+
+        public void insertCoin(int vmIndex, Coin coin)
+        {
+
+            int acceptedCoin = 0;
+            foreach (int coinType in vm_List[vmIndex].get_vmCoins())
+            {
+                if (coinType == coin.Value)
+                {
+                    acceptedCoin++;
+                    vm_List[vmIndex].insertChute(coin);
+                    vm_List[vmIndex].insertCoin(coin.Value);
+                }
+            }
+
+            if (acceptedCoin != 1)
+            {
+                vm_List[vmIndex].deliveryChuteCoin(coin);
+            }
+        }
+        public void pressButton(int vmIndex, int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Invalid button press, button is negative!");
+            }
+
+            if (value > vm_List[vmIndex].get_vmButtons())
+            {
+                throw new Exception("Invalid button press, button is higher than the number of buttons on the vending machine!");
+            }
+
+            int insertedTotal = vm_List[vmIndex].insertCoinTotal();
+            List<int> popCost = vm_List[vmIndex].get_vmPopCosts();
+            List<Pop>[] popName = vm_List[vmIndex].get_PopChute();
+
+            if (insertedTotal >= popCost[value])
+            {
+                
+                int change = insertedTotal - popCost[value];
+                
+                if (popName[value].Count() >= 1)
+                {
+                    vm_List[vmIndex].insertCoinEarn();
+                    vm_List[vmIndex].give_Change(change);
+                    vm_List[vmIndex].deliveryChutePop(value);
+                    vm_List[vmIndex].clear_insertCoins();        
+                }
+            }
+        }
+        public List<Deliverable> extractFromDeliveryChute(int vmIndex)
+        {
+            List<Deliverable> vm_DeliveryChute = new List<Deliverable>(vm_List[vmIndex].get_deliveryChute());
+            vm_List[vmIndex].clear_deliveryChute();
+            return vm_DeliveryChute;
+        }
+        public List<IList> unloadVendingMachine(int vmIndex)
+        {
+            return vm_List[vmIndex].unloadVM();
+        }
+    }
+}
